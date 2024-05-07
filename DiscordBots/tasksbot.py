2fd605d7c626af9d878d6db059a04c6823d48672
Aditya Shibu <as2397@hw.ac.uk>
2024-05-07 16:26:17 +0400

    Added slash commands

--- DiscordBots/tasksbot.py
+++ DiscordBots/tasksbot.py
@@ -1,46 +1,25 @@
-import discord 
+import discord
+from discord import app_commands
 from discord.ext import commands
-from datetime import datetime
-from token import DISCORD_TOKEN
+from my_token import DISCORD_TOKEN
 
-# Bot prefix
-bot = commands.Bot(command_prefix='!')
-
-# Tasks dictionary to keep track of all the tasks
-tasks = {}
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}')
+    print("Bot is up and ready")
+    try: 
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+        
+@bot.tree.command(name="hello")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hey {interaction.user.mention}! This is a slash command!", ephemeral=True)
     
-@bot.command()
-async def createtask(ctx, assignee: discord.Member, due_date: str, *, task_description: str):
-    try:
-        # Parse the string into a datetime object
-        due_date = datetime.strptime(due_date, '%d-%m-%Y')
-        
-        # Create task dictionary
-        task = {
-            'assignee': assignee,
-            'due_date': due_date,
-            'description': task_description
-        }
-        
-        tasks[ctx.channel.id] = task
-        
-        # Send message in the channel
-        await ctx.send(f'Task created: {task_description} | Assignee: {assignee} | Due date: {due_date}')
-        
-    except ValueError:
-        await ctx.send('Invalid date format. Please use DD-MM-YYYY.')
-        
-@bot.command()
-async def showtasks(ctx):
-    task_list = ""
-    for channel_id, task in tasks.items():
-        if channel_id == ctx.channel.id:
-            task_list += f"Assignee: {task['assignee']} | Due Date: {task['due_date']} | Description: {task['description']}\n"
-    await ctx.send(task_list)
+@bot.tree.command(name="name")
+async def name(interaction:discord.Interaction):
+    await interaction.response.send_message(f"Your name is {interaction.user.mention}!")
 
-# Run the bot
 bot.run(DISCORD_TOKEN)
